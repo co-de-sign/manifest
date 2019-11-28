7d0b1ebf121d1ce4852cade2c4568914ced22dea
Petterson Alves <pettalves@gmail.com>
2019-11-28 14:43:35 -0300

    Improve js

--- script.js
+++ script.js
@@ -1,45 +1,42 @@
-const what = [
-  'uma comunidade',
-  'um rolê',
-  'um movimento'
-];
-
-const how = [
-  'anárquica',
-  'assíncrona',
-  'remota',
-  'online',
-  'antifascista',
-  'pós-gênero',
-  'coletivista',
-  'democrática',
-  'transparente'
-];
-
-const goal = [
-  'revolucionar',
-  'destruir',
-  'doutrinar',
-  'compartilhar'
-];
+const sources = {
+  what: [
+    'uma comunidade',
+    'um rolê',
+    'um movimento'
+  ],
+  how: [
+    'anárquica',
+    'assíncrona',
+    'remota',
+    'online',
+    'antifascista',
+    'pós-gênero',
+    'coletivista',
+    'democrática',
+    'transparente'
+  ],
+  goal: [
+    'revolucionar',
+    'destruir',
+    'doutrinar',
+    'compartilhar'
+  ],
+}
 
 const colors = [
   '#FF6699',
   '#FF9966',
-  '#FFFF66',
+  '#FFC107',
   '#00BD97',
   '#99CCFF',
   '#9966CC'
 ];
 
-var whatId = document.getElementById('what');
-var howId = document.getElementById('how');
-var goalId = document.getElementById('goal');
+const elementWord = document.querySelectorAll(".word");
 
-function randomize (arr) {
-  var unique = true;
-  var item = Math.floor(Math.random() * arr.length - 5);
-  value = arr.splice(item,1);
+function randomize(arr) {
+  const item = Math.floor(Math.random() * arr.length - 5);
+  const value = arr.splice(item, 1);
   arr.push(value);
 
   return value;
@@ -49,38 +46,17 @@ function setColor(id) {
   id.style.setProperty('--color', randomize(colors));
 }
 
-function setWhat() {
-  const word = randomize(what);
-  setColor(whatId);
+function setChange() {
+  for (var i = 0; i < elementWord.length; i++) {
+    elementWord[i].addEventListener('click', function () {
+      setColor(this);
 
-  whatId.innerHTML = word;
+      const word = randomize(sources[this.id]);
+      this.innerHTML = word;
+    }, false);
+
+    elementWord[i].click();
+  }
 }
 
-function setHow() {
-  const word = randomize(how);
-  setColor(howId);
-
-  howId.innerHTML = word;
-}
-
-function setGoal() {
-  const word = randomize(goal);
-  setColor(goalId)
-
-  goalId.innerHTML = word;
-}
-
-whatId.addEventListener('click', function() {
-  setWhat();
-  setColor(whatId);
-}, false)
-
-howId.addEventListener('click', function() {
-  setHow();
-}, false)
-
-goalId.addEventListener('click', function() {
-  setGoal();
-}, false)
-
-window.onload = () => setWhat(); setHow(); setGoal();
+window.onload = () => setChange();

2149b29cad7889fdb20641aea7712dcfdefb84a5
LORDBABUINO <jorge.x7@gmail.com>
2019-12-02 16:41:09 -0300

    Fix randomization

--- script.js
+++ script.js
@@ -34,21 +34,31 @@ const colors = [
 
 const elementWord = document.querySelectorAll(".word");
 
-function randomize(arr) {
-  const item = Math.floor(Math.random() * arr.length - 5);
-  const value = arr.splice(item, 1);
-  arr.push(value);
+function randomize(arr, last) {
+  const filteredArr = arr.filter((value) => value !== last)
+  const item = Math.floor(Math.random() * filteredArr.length);
 
-  return value;
+  return filteredArr[item];
+}
+
+function intToHex(c) {
+  const hex = c.toString(16).toUpperCase();
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(rgb) {
+  const arr = eval('[' + rgb.slice(4,-1) + ']')
+  return "#" + intToHex(arr[0]) + intToHex(arr[1]) + intToHex(arr[2]);
 }
 
 function setColor(id) {
-  id.style.backgroundColor = randomize(colors);
+  const currentColor = rgbToHex(getComputedStyle(id).backgroundColor)
+  id.style.backgroundColor = randomize(colors, currentColor);
 }
 
 function populate(element) {
   setColor(element);  
-  element.innerHTML = randomize(sources[element.id]);
+  element.textContent = randomize(sources[element.id], element.textContent);
 }
 
 function setChange() {
